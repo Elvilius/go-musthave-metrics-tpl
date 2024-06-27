4c9520177228b7e6a39ed80e52293bcbcdfbd81e
Sergienko Victor <vik.sergienko@mail.ru>
2024-06-27 21:58:25 +0200

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,28 @@ import (
 	handler "github.com/Elvilius/go-musthave-metrics-tpl/internal/handlers"
 	"github.com/Elvilius/go-musthave-metrics-tpl/internal/server"
 	"github.com/Elvilius/go-musthave-metrics-tpl/internal/storage"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	sugarLogger := logger.Sugar()
+
 	memStorage := storage.NewMemStorage()
 	handler := handler.NewHandler(memStorage)
 	cfg := config.GetServerConfig()
-	server := server.New(&cfg, handler)
+	server := server.New(&cfg, handler, sugarLogger)
 
 	server.Run()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,19 +5,22 @@ import (
 
 	"github.com/Elvilius/go-musthave-metrics-tpl/internal/config"
 	handler "github.com/Elvilius/go-musthave-metrics-tpl/internal/handlers"
+	middleware "github.com/Elvilius/go-musthave-metrics-tpl/internal/midleware"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 type Server struct {
 	handler *handler.Handler
 	router  *chi.Mux
 	cfg     *config.ServerConfig
+	logger  *zap.SugaredLogger
 }
 
-func New(cfg *config.ServerConfig, handler *handler.Handler) *Server {
+func New(cfg *config.ServerConfig, handler *handler.Handler, logger *zap.SugaredLogger) *Server {
 	router := chi.NewRouter()
 
-	server := &Server{handler: handler, router: router, cfg: cfg}
+	server := &Server{handler: handler, router: router, cfg: cfg, logger: logger}
 
 	router.Get("/", server.handler.All)
 	router.Post("/update/{metricType}/{metricName}/{metricValue}", server.handler.Update)
@@ -27,7 +30,7 @@ func New(cfg *config.ServerConfig, handler *handler.Handler) *Server {
 }
 
 func (s *Server) Run() {
-	err := http.ListenAndServe(s.cfg.Address, s.router)
+	err := http.ListenAndServe(s.cfg.Address, middleware.Logging(s.router, s.logger))
 	if err != nil {
 		panic(err)
 	}
