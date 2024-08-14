c1d433ae9199175fbfcb3580e44281fd755d3528
Sergienko Victor <vik.sergienko@mail.ru>
2024-08-14 19:06:09 +0200

    Fix

--- internal/server/server.go
+++ internal/server/server.go
@@ -34,7 +34,7 @@ func New(cfg *config.ServerConfig, handler *handler.Handler, logger *zap.Sugared
 	router.Post("/update/", server.handler.UpdateJSON)
 	router.Get("/value/{type}/{id}", server.handler.Value)
 	router.Post("/value/", server.handler.ValueJSON)
-	router.Post("/updates", server.handler.UpdatesJSON)
+	router.Post("/updates/", server.handler.UpdatesJSON)
 
 	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
