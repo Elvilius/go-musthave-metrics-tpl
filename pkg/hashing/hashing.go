c4b6ea95aca9e19c5db31be787beb388adf502a3
Sergienko Victor <vik.sergienko@mail.ru>
2024-08-24 19:28:25 +0200

    wip

--- pkg/hashing/hashing.go
+++ pkg/hashing/hashing.go
@@ -1,9 +1,19 @@
 package hashing
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+)
+
 func GenerateHash(key string, data []byte) string {
-	return "123123123"
+	h := hmac.New(sha256.New, []byte(key))
+	h.Write(data)
+	sum := h.Sum(nil)
+	return base64.StdEncoding.EncodeToString(sum)
 }
 
 func VerifyHash(key string, data []byte, encryptedHash string) bool {
-	return true
+	hash := GenerateHash(key, data)
+	return hash == encryptedHash
 }
--- pkg/hashing/hashing_test.go
+++ pkg/hashing/hashing_test.go
@@ -12,3 +12,10 @@ func TestHashingTrue(t *testing.T) {
 	ok := VerifyHash("secret", data, hash)
 	assert.True(t, ok)
 }
+
+func TestHashingFalse(t *testing.T) {
+	data := []byte("test data")
+	hash := GenerateHash("secret", data)
+	ok := VerifyHash("secrets", data, hash)
+	assert.False(t, ok)
+}
