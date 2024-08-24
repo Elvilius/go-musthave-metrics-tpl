371463d94b20d33623ed65704ae1acaa9b9c9605
Sergienko Victor <vik.sergienko@mail.ru>
2024-08-24 21:59:41 +0200

    Fix

--- pkg/midleware/middleware.go
+++ pkg/midleware/middleware.go
@@ -1,15 +1,15 @@
 package middleware
 
 import (
-	"bytes"
-	"io"
+	_"bytes"
+	_"io"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/Elvilius/go-musthave-metrics-tpl/internal/config"
 	"github.com/Elvilius/go-musthave-metrics-tpl/pkg/gzip"
-	"github.com/Elvilius/go-musthave-metrics-tpl/pkg/hashing"
+	_"github.com/Elvilius/go-musthave-metrics-tpl/pkg/hashing"
 	"go.uber.org/zap"
 )
 
@@ -93,18 +93,18 @@ func Gzip(h http.Handler) http.Handler {
 }
 func VerifyHash(cfg *config.ServerConfig, logger zap.SugaredLogger, next http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if cfg.Key != "" {
-			data, err := io.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			if ok := hashing.VerifyHash(cfg.Key, data, r.Header.Get("HashSHA256")); !ok {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			r.Body = io.NopCloser(bytes.NewBuffer(data))
-		}
+		// if cfg.Key != "" {
+		// 	data, err := io.ReadAll(r.Body)
+		// 	if err != nil {
+		// 		w.WriteHeader(http.StatusBadRequest)
+		// 		return
+		// 	}
+		// 	if ok := hashing.VerifyHash(cfg.Key, data, r.Header.Get("HashSHA256")); !ok {
+		// 		w.WriteHeader(http.StatusBadRequest)
+		// 		return
+		// 	}
+		// 	r.Body = io.NopCloser(bytes.NewBuffer(data))
+		// }
 
 		next.ServeHTTP(w, r)
 	})
