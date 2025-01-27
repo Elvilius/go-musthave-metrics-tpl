fae78c9a290adb37fd0211cc4da2fdd71c5a49fc
Victor <vik.sergienko@mail.ru>
2025-01-27 21:15:14 +0100

    Fix

--- internal/storage/file_storage.go
+++ internal/storage/file_storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -26,24 +27,30 @@ func (f *FileStorage) SaveToFile() error {
 		return err
 	}
 	path := filepath.Join(wd, f.cfg.FileStoragePath)
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create temp file: %w", err)
 	}
 	defer file.Close()
 
 	metrics, err := f.storage.GetAll(context.TODO())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get metrics: %w", err)
 	}
+
 	bytes, err := json.Marshal(metrics)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal metrics: %w", err)
 	}
+
 	_, err = file.Write(bytes)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to write to temp file: %w", err)
+	}
+
+	if err := file.Close(); err != nil {
+		return fmt.Errorf("failed to close temp file: %w", err)
 	}
 
 	return nil
