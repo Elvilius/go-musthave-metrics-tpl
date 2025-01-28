ca09c133fded434c14264fa9af04ea8ed063a351
Victor <vik.sergienko@mail.ru>
2025-01-28 09:36:44 +0100

    Fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -54,7 +54,7 @@ func NewAgent() *AgentConfig {
 		ServerAddress:  getEnvOrDefaultString("ADDRESS", "localhost:8080"),
 		PollInterval:   getEnvOrDefaultInt("POLL_INTERVAL", 3),
 		ReportInterval: getEnvOrDefaultInt("REPORT_INTERVAL", 10),
-		Key:            getEnvOrDefaultString("KEY", "test"),
+		Key:            getEnvOrDefaultString("KEY", ""),
 		RateLimit:      getEnvOrDefaultInt("RATE_LIMIT", 3),
 	}
 
